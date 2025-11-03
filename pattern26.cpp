a009c3a2419e4d0abf18168368a047161b66e770
Vikassen1 <vikassen472@gmail.com>
2025-11-03 17:00:06 +0530

    3/11/25

---
+++ pattern26.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+   
+    // 🔼 Upper part
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        cout << "*";
+        
+        if(i != 1) {
+            for(int j = 1; j <= 2*i - 3; j++) {
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // 🔽 Lower part
+    for(int i = n - 1; i >= 1; i--) {
+        for(int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        cout << "*";
+        
+        if(i != 1) {
+            for(int j = 1; j <= 2*i - 3; j++) {
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
