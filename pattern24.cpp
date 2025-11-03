70e36ba7b385474862a8ec1e37fc0f40e080ead7
Vikassen1 <vikassen472@gmail.com>
2025-11-03 16:02:37 +0530

    3/11/25

---
+++ pattern24.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int row=4;
+int col=4;
+
+  for(int i=1;i<=row;i++){
+    for(int j=1;j<=col;j++){
+        if(i==1 || j==1 || i==row || j==col){
+            cout<<"*";
+        }else{
+            cout<<" ";
+        }
+    }
+    cout<<endl;
+  }
+return 0;
+}
